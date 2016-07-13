9c117bc1eea1a2e830a7bb86802eab20217e7792
zangyanming <zyanming2002@163.com>
2016-07-13 15:48:13 +0800

    新增流程中结束表单的抽象类。

---
+++ workflow-parent/middleware-workflow-biz/src/main/java/com/kalix/middleware/workflow/biz/AbstractEndFormHandler.java
@@ -0,0 +1,43 @@
+package com.kalix.middleware.workflow.biz;
+
+
+import com.kalix.middleware.workflow.api.biz.IFormHandler;
+
+/**
+ * Created by Administrator on 2016/6/2.
+ */
+public abstract class AbstractEndFormHandler implements IFormHandler {
+    private String processDefinitionId;
+
+    public AbstractEndFormHandler(){
+        String[] splits=this.getClass().getSimpleName().split("End");
+
+        if(splits.length>0) {
+            processDefinitionId=splits[0].toLowerCase();
+        }
+    }
+
+    @Override
+    public String getProcessDefinitionId() {
+        return processDefinitionId;
+    }
+    /**
+     * 获得form的key
+     *
+     * @return
+     */
+    @Override
+    public String getFormKey() {
+        return "end.form";
+    }
+
+    /**
+     * 获得extjs的窗口类名称
+     *
+     * @return
+     */
+    @Override
+    public String getWindowClass() {
+        return "";
+    }
+}
