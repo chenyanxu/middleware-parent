a9cfc942fe4a9a85a2b803d23ff346a80392a75c
sunlingfeng <minikiller@qq.com>
2016-10-11 21:38:59 +0800

    change grid column.

--- workflow-parent/middleware-workflow-extjs/src/main/webapp/workflow/processdefinition/view/ProcessDefinitionGrid.js
+++ workflow-parent/middleware-workflow-extjs/src/main/webapp/workflow/processdefinition/view/ProcessDefinitionGrid.js
@@ -20,71 +20,71 @@ Ext.define('kalix.workflow.processdefinition.view.ProcessDefinitionGrid', {
         type: 'processDefinitionStore'
     },
     columns: [
-            {
-                xtype: "rownumberer",
-            },
-            {
-                text: '流程定义编号',
-                dataIndex: 'id',
-            },
-            {
-                text: '流程定义名称',
-                dataIndex: 'name'
-            },
-            {
-                text: '关键字',
-                dataIndex: 'key'
-            },
-            {
-                text: '描述',
-                dataIndex: 'description'
-            },
-            {
-                text: '版本',
-                dataIndex: 'version'
-            },
-            {
-                text: '状态', dataIndex: 'suspensionState',
-                renderer: function (value) {
-                    if (value == 1)
-                        return "有效";
-                    else
-                        return "无效";
-                }
-            },
-            {
-                xtype: "securityGridColumnCommon",
-                verifyItems: [
-                    /*{
-                     iconCls: 'iconfont icon-edit-column',
-                     tooltip: '编辑',
-                     handler: 'onEdit',
-                     permission: 'edit'
-                     },*/
-                    {
-                        itemId: 'activateButton',
-                        getClass: function (v, meta, record) {
-                            if (record.data.suspensionState == 1) {
-                                return "iconfont icon-stop";
-                            }
-                            return "iconfont icon-start";
-                        },
-                        getTip: function (value, metadata, record, row, col, store) {
-                            if (record.data.suspensionState == 1) {
-                                return "无效";
-                            }
-                            return '有效';
-                        },
-                        handler: 'onIsActivate',
-                        permission: 'activate'
-                    },
-                    {
-                        iconCls: 'iconfont icon-view-column',
-                        tooltip: '查看',
-                        handler: 'onOpenProcessDefinition',
-                        permission: 'view'
-                    }]
+        {
+            xtype: "rownumberer",
+        },
+        {
+            text: '流程定义编号',
+            dataIndex: 'id',
+        },
+        {
+            text: '流程定义名称',
+            dataIndex: 'name'
+        },
+        {
+            text: '关键字',
+            dataIndex: 'key'
+        },
+        {
+            text: '描述',
+            dataIndex: 'description'
+        },
+        {
+            text: '版本',
+            dataIndex: 'version'
+        },
+        {
+            text: '状态', dataIndex: 'suspensionState',
+            renderer: function (value) {
+                if (value == 1)
+                    return "有效";
+                else
+                    return "无效";
             }
+        },
+        {
+            xtype: "securityGridColumnCommon",
+            verifyItems: [
+                /*{
+                 iconCls: 'iconfont icon-edit-column',
+                 tooltip: '编辑',
+                 handler: 'onEdit',
+                 permission: 'edit'
+                 },*/
+                {
+                    itemId: 'activateButton',
+                    getClass: function (v, meta, record) {
+                        if (record.data.suspensionState == 1) {
+                            return "iconfont icon-stop";
+                        }
+                        return "iconfont icon-start";
+                    },
+                    getTip: function (value, metadata, record, row, col, store) {
+                        if (record.data.suspensionState == 1) {
+                            return "无效";
+                        }
+                        return '有效';
+                    },
+                    handler: 'onIsActivate',
+                    permission: 'activate'
+                },
+                {
+                    iconCls: 'iconfont icon-view-column',
+                    tooltip: '查看',
+                    handler: 'onOpenProcessDefinition',
+                    permission: 'view'
+                }]
+        }
     ],
     tbar: {
         xtype: 'securityToolbar',
