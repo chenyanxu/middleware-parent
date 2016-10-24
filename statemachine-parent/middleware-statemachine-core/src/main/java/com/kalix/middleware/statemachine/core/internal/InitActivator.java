763234e13b633b47959b398bb87e5ba620661b14
chenyanxu <yanxu2016>
2016-10-24 08:39:02 +0800

    use base bundle activator

--- statemachine-parent/middleware-statemachine-core/src/main/java/com/kalix/middleware/statemachine/core/internal/InitActivator.java
+++ statemachine-parent/middleware-statemachine-core/src/main/java/com/kalix/middleware/statemachine/core/internal/InitActivator.java
@@ -1,5 +1,6 @@
 package com.kalix.middleware.statemachine.core.internal;
 
+import com.kalix.framework.core.api.osgi.BaseBundleActivator;
 import com.kalix.framework.core.util.SystemUtil;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
@@ -7,18 +8,18 @@ import org.osgi.framework.BundleContext;
 /**
  * Created by sunlf on 14-3-23.
  */
-public class InitActivator implements BundleActivator {
+public class InitActivator extends BaseBundleActivator {
 
     private static BundleContext context;
     @Override
     public void start(BundleContext bundleContext) throws Exception {
-        SystemUtil.startBundlePrintln(bundleContext);
+        super.start(bundleContext);
         context=bundleContext;
     }
 
     @Override
     public void stop(BundleContext bundleContext) throws Exception {
-        SystemUtil.stopBundlePrintln(bundleContext);
+        super.stop(bundleContext);
         context=null;
     }
 
