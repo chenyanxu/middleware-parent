56a8e6ff27f5ae34106cd23b013f7abf82928ae5
zangyanming <zyanming2002@163.com>
2016-11-16 14:18:03 +0800

    流程历史增加启动用户和发起时间查询条件

--- workflow-parent/middleware-workflow-extjs/src/main/webapp/workflow/processhistory/view/ProcessHistorySearchForm.js
+++ workflow-parent/middleware-workflow-extjs/src/main/webapp/workflow/processhistory/view/ProcessHistorySearchForm.js
@@ -16,6 +16,36 @@ Ext.define('kalix.workflow.processhistory.view.ProcessHistorySearchForm', {
             labelWidth: 60,
             width: 200,
             name: 'name'
+        },
+        {
+            xtype: 'textfield',
+            fieldLabel: '启动用户',
+            labelAlign: 'right',
+            labelWidth: 60,
+            width: 200,
+            name: 'startUserId'
+        },
+        {
+            xtype: 'datefield',
+            format: 'Y-m-d',
+            fieldLabel: '发起时间:',
+            labelAlign: 'right',
+            labelWidth: 60,
+            width: 200,
+            name: 'startTime:begin:gt'
+        },
+        {
+            xtype: 'label',
+            text: '-',
+            margin: '5 5 0 5'
+        },
+        {
+            xtype: 'datefield',
+            format: 'Y-m-d',
+            headLabel: true,
+            labelAlign: 'right',
+            width: 140,
+            name: 'startTime:end:lt'
         }
     ]
 });
