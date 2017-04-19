fef60f9361dea57ec7967183ee675cd34ed7ff35
sunlingfeng <minikiller@qq.com>
2017-04-20 06:31:35 +0800

    format client demo output

--- oauth-parent/middleware-oauth-biz/src/main/java/com/kalix/middleware/oauth/client/UrlClient.java
+++ oauth-parent/middleware-oauth-biz/src/main/java/com/kalix/middleware/oauth/client/UrlClient.java
@@ -7,6 +7,7 @@ import org.apache.oltu.oauth2.common.message.types.ResponseType;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
@@ -14,7 +15,8 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
- * Created by Administrator on 2017-04-14.
+ * Created by sunlf on 2017-04-14.
+ * oauth2的client测试代码，演示验证的整个过程
  */
 public class UrlClient {
     private static String getAuthCode() throws Exception {
@@ -28,14 +30,7 @@ public class UrlClient {
 
         StringBuilder postStr = new StringBuilder();
 
-        for (Map.Entry<String, Object> param : params.entrySet()) {
-            if (postStr.length() != 0) {
-                postStr.append('&');
-            }
-            postStr.append(URLEncoder.encode(param.getKey(), "UTF-8"));
-            postStr.append('=');
-            postStr.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
-        }
+        processUrl(params, postStr);
 
         byte[] postStrBytes = postStr.toString().getBytes("UTF-8");
 
@@ -50,8 +45,22 @@ public class UrlClient {
 
         connection.setInstanceFollowRedirects(false);// 必须设置该属性
         String location = connection.getHeaderField("Location");
-        System.out.println(location);
+        System.out.println("第一步：请求授权码");
+        System.out.println("请求地址:" + ClientParams.OAUTH_SERVER_URL + postStr.toString());
+
+        System.out.println("返回结果:" + location);
         return location.substring(location.indexOf("=") + 1);
+    }
+
+    private static void processUrl(Map<String, Object> params, StringBuilder postStr) throws UnsupportedEncodingException {
+        for (Map.Entry<String, Object> param : params.entrySet()) {
+            if (postStr.length() != 0) {
+                postStr.append('&');
+            }
+            postStr.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+            postStr.append('=');
+            postStr.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+        }
     }
 
     /**
@@ -70,14 +79,7 @@ public class UrlClient {
 
         StringBuilder postStr = new StringBuilder();
 
-        for (Map.Entry<String, Object> param : params.entrySet()) {
-            if (postStr.length() != 0) {
-                postStr.append('&');
-            }
-            postStr.append(URLEncoder.encode(param.getKey(), "UTF-8"));
-            postStr.append('=');
-            postStr.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
-        }
+        processUrl(params, postStr);
 
         byte[] postStrBytes = postStr.toString().getBytes("UTF-8");
 
@@ -97,11 +99,13 @@ public class UrlClient {
             response.append(inputLine);
         }
         in.close();
-        System.out.println(response.toString());
+
         Gson gson = new GsonBuilder().create();
         Map<String, String> map = gson.fromJson(response.toString(), Map.class);
         String accessToken = map.get("access_token");
-        System.out.println(accessToken);
+        System.out.println("第二步：换取访问令牌");
+        System.out.println("请求地址:" + ClientParams.OAUTH_SERVER_URL + postStr.toString());
+        System.out.println("返回结果:" + response.toString());
         return accessToken;
     }
 
@@ -112,8 +116,6 @@ public class UrlClient {
      */
     private static void getService(String accessToken) throws Exception {
         String uri = ClientParams.OAUTH_SERVICE_API + "?access_token=" + accessToken;
-        System.out.println("demo address is :" + uri);
-
         URL url = new URL(uri);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod(OAuth.HttpMethod.GET);
@@ -127,9 +129,10 @@ public class UrlClient {
             response.append(inputLine);
         }
         in.close();
-        System.out.println(response.toString());
+        System.out.println("第三步：测试accessToken");
+        System.out.println("请求地址:" + uri);
+        System.out.println("返回结果:" + response.toString());
     }
-
 
 
     public static void main(String[] args) throws Exception {
