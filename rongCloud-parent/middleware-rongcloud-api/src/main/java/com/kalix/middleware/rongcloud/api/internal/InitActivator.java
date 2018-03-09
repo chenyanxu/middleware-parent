17ef628b3e82e671394850875f6d5981e609d41f
fengweibin <tx_xinxin2006@163.com>
2018-03-09 13:39:10 +0800

    增加融云接口

---
+++ rongCloud-parent/middleware-rongcloud-api/src/main/java/com/kalix/middleware/rongcloud/api/internal/InitActivator.java
@@ -0,0 +1,9 @@
+package com.kalix.middleware.rongcloud.api.internal;
+
+import com.kalix.framework.core.api.osgi.BaseBundleActivator;
+
+/**
+ * Created by sunlf on 14-3-23.
+ */
+public class InitActivator extends BaseBundleActivator {
+}
