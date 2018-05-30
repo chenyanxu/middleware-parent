11fcf8d0e2cae80133a876b75e80a5e266581142
fengweibin <tx_xinxin2006@163.com>
2018-05-30 09:41:41 +0800

    移除OSGI-INF文件夹，增加refreshToken方法

--- jwt-parent/middleware-jwt-api/src/main/java/com/kalix/middleware/jwt/api/biz/IJwtService.java
+++ jwt-parent/middleware-jwt-api/src/main/java/com/kalix/middleware/jwt/api/biz/IJwtService.java
@@ -8,8 +8,8 @@ import io.jsonwebtoken.Claims;
  * @author fwb
  */
 public interface IJwtService extends IService {
-//    public io.jsonwebtoken.Claims parseJWT(String jsonWebToken, String base64Security);
-//    public  String createJWT(String name, String userId, String role,
-//                             String audience, String issuer, long TTLMillis, String base64Security);
-//    public AudienceBean getAudien();
+    public io.jsonwebtoken.Claims parseJWT(String jsonWebToken, String base64Security);
+    public  String createJWT(String name, String userId, String role,
+                             String audience, String issuer, long TTLMillis, String base64Security);
+    public AudienceBean getAudien();
 }
