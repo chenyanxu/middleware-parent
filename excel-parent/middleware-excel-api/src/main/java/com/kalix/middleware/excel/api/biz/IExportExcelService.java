34a16bd817770ecf3eff08db41323abd60cb6477
houqingjie <150091225@qq.com>
2018-09-26 11:18:18 +0800

    新增Excel导出接口和实现框架

---
+++ excel-parent/middleware-excel-api/src/main/java/com/kalix/middleware/excel/api/biz/IExportExcelService.java
@@ -0,0 +1,11 @@
+package com.kalix.middleware.excel.api.biz;
+
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author fengweibin
+ */
+public interface IExportExcelService extends IExcelService {
+}
---
+++ excel-parent/middleware-excel-biz/src/main/java/com/kalix/middleware/excel/biz/ExportExcelServiceImpl.java
@@ -0,0 +1,12 @@
+package com.kalix.middleware.excel.biz;
+
+import com.kalix.middleware.excel.api.biz.IExportExcelService;
+
+/**
+ * @author fengweibin
+ */
+public class ExportExcelServiceImpl extends ExcelServiceImpl implements IExportExcelService {
+
+    public ExportExcelServiceImpl() {
+    }
+}
