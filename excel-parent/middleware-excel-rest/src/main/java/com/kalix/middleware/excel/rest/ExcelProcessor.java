1ee8457545a82c48d53b9746fd68f68d59e530f2
fengweibin <tx_xinxin2006@163.com>
2018-11-20 09:57:09 +0800

    填空题导入保存spacenum字段

--- excel-parent/middleware-excel-rest/src/main/java/com/kalix/middleware/excel/rest/ExcelProcessor.java
+++ excel-parent/middleware-excel-rest/src/main/java/com/kalix/middleware/excel/rest/ExcelProcessor.java
@@ -114,7 +114,7 @@ public class ExcelProcessor implements Processor {
                             map.remove("version");
                             if(ServiceUrl.toLowerCase().indexOf("completion")>-1){
                                String stem= map.get("stem").toString();
-                                int num=getSpaceNum(stem);
+                                int num=getSpaceNum(stem,"[#");
                                 map.put("spaceNum",String.valueOf(num));
                             }
                             HttpClientUtil.shiroPost(ServiceUrl, map, sessionId, access_token);
@@ -138,16 +138,11 @@ public class ExcelProcessor implements Processor {
     }
 
 
-    public int getSpaceNum(String stem) {
-        String pattern = "(\\[#).*?(\\])";
-        // 编译正则
-        Pattern p1 = Pattern.compile(pattern);
-        // 指定要匹配的内容
-        Matcher m = p1.matcher(stem);
-        m.replaceAll("_");
-        // 计算次数
-        int count = 0;
-        while (m.find()) {
+    public int getSpaceNum(String stem,String findText) {
+        int count=0;
+        int index = 0;
+        while ((index = stem.indexOf(findText, index)) != -1) {
+            index = index + findText.length();
             count++;
         }
        return  count;
